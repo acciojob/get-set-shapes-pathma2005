cb76f68c7e911a8fe33f439c456212de972064df
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-15 17:30:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //complete this code
 class Rectangle {
 	constructor(width,height){
-		this.width=width;
-		this.height=height;
+		this._width=width;
+		this._height=height;
 	}
 	get width(){
-		return this.width;
+		return this._width;
 	}
 	get height(){
-		return this.height;
+		return this._height;
 	}
 	getArea(){
-		return this.width*this.height;
+		return this._width*this._height;
 	}
 }
 class Square extends Rectangle {
@@ -19,7 +19,7 @@ class Square extends Rectangle {
 		super(side,side);
 	}
 	getPerimeter(){
-		return 4*this.width;
+		return 4*this._width;
 	}
 }
 
